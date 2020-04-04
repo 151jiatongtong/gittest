385d85599c7f408ad795871551ae6e1215a5e638
tom_glb <goodmorning_glb@tong.com>
2020-04-04 14:33:31 +0800

    提交

--- hw.py
+++ hw.py
@@ -1,51 +1,55 @@
-import json
 
 all_record = []
 id_record = 0
-dic = {}
+
 
 def add_record():
     name = input("请输入姓名：")
     phone = input("请输入电话：")
     global id_record
     id_record += 1
+    dic = {}
     dic["name"] = name
     dic["phone"] = phone
     dic["id"] = id_record
+    all_record.append(dic)
     print("添加成功：", dic)
+    return all_record
+
+
 
 def query_record():
     name = input("请输入姓名：")
-    with open("record.txt", "r") as f:
-        for i in json.loads(f.read()):
-            if name == i.get("name"):
-                print(i)
-
+    for listname in all_record:
+        if listname["name"] == name:
+            print("查询结果：", listname)
+        else:
+            print("查询失败")
 
 def change_record():
     old = input("请输入要修改人姓名：")
     change = int(input("请选择要修改的信息：1.name 2.phone"))
     new = input("请输入新的name 或 phone")
-    with open("record.txt", "r") as f:
-        for i in json.loads(f.read()):
-            if old == i.get("name"):
-                if change == 1:
-                    i["name"] = new
-                else:
-                    i["phone"] = new
+    for i in all_record:
+        if old == i.get("name"):
+            if change == 1:
+                i["name"] = new
+            else:
+                i["phone"] = new
+        else:
+            print("姓名不存在")
     print("修改成功")
 
 def delete_record():
     name = input("请输入name：")
-    with open("record.txt", "r") as f:
-        for i in json.loads(f.read()):
-            if name == i["name"]:
-                newlist = json.loads(f.read()).remove(i)
-                with open("record.txt", "w") as f:
-                    f.write(json.dumps(newlist))
-    print("删除成功")
+    for i in all_record:
+        if name == i["name"]:
+            all_record.remove(i)
+            print("删除成功：", all_record)
+        else:
+            print("删除失败")
 
-add_record()
+
 
 def main():
     while True:
@@ -71,4 +75,4 @@ def main():
             print("退出")
             break
 
-# main()
+main()
